9d37adf1c57749fac66f69f465a72c3dc6937065
Daniel <daniel@bfydigital.com>
2022-02-11 13:38:16 +0100

    modified tests for new functionality
    removed tests for completion contract, because it is no longer required

--- test/staker.js
+++ test/staker.js
@@ -1,4 +1,5 @@
 const { assert } = require("chai");
+const BFYToken = artifacts.require("BFYToken");
 const Staker = artifacts.require("Staker");
 
 contract("Staker", function (accounts) {
@@ -7,7 +8,8 @@ contract("Staker", function (accounts) {
   let staker;
 
   beforeEach(async () => {
-    staker = await Staker.new();
+    bfyToken = await BFYToken.new();
+    staker = await Staker.new(bfyToken.address);
   });
 
   it("should allow user to skate when status is open", async () => {
@@ -23,6 +25,16 @@ contract("Staker", function (accounts) {
     // assert
     assert.isTrue(actual.eq(expected), "actual and expected staked values do not match");
     assert.isTrue(actualStakersNo.eq(web3.utils.toBN(1)), "actual number of stakers does not match the expected '1'")
+  });
+
+  it("should not allow users to stake when status is not open", async () => {
+    // arrange
+    let amount = web3.utils.toWei('0.5', 'ether');
+
+    // act + assert
+    await staker.closeStaking({ from: accounts[0] });
+    await Exception.tryCatch(
+      staker.stake({ from: accounts[0], value: amount }), Exception.errTypes.stakingNotOpen);
   });
 
   it("should allow only the owner to close staking", async () => {
@@ -47,14 +59,14 @@ contract("Staker", function (accounts) {
     // act
     let firstStakedAmount = await staker.getTotalStakedAmount.call({ from: accounts[0] });
     await staker.stake({ from: accounts[0], value: amount });
-    let secondStakedAmount = await staker.getTotalStakedAmount.call({ from: accounts[0] });
+    let secondStakedAmount = await staker.getTotalStakedAmount.call({ from: accounts[1] });
 
     // assert
     assert.isTrue(firstStakedAmount.eq(web3.utils.toBN(0)));
     assert.isTrue(secondStakedAmount.eq(web3.utils.toBN(amount)));
   });
 
-  it("should refund users on withdrawals", async () => {
+  it("should refund users on withdrawals when staking is closed", async () => {
     // arrange
     let amount = web3.utils.toWei('1', 'ether');
 
@@ -126,49 +138,27 @@ contract("Staker", function (accounts) {
       staker.completeStaking({ from: accounts[1] }), Exception.errTypes.nonOwnerCall);
   });
 
-  it("should allow owner to award staked balance when skating is complete", async () => {
+  it("should restart staking when staking is complete", async () => {
     // arrange
-    let amount = web3.utils.toWei('10', 'ether');
-    let sender = accounts[0];
-    let receiver = accounts[1];
-    let initialBalance = web3.utils.toBN(await web3.eth.getBalance(receiver));
+    let amount = web3.utils.toWei('2', 'ether');
 
-    // act
-    await staker.stake({ from: sender, value: amount });
-    await staker.completeStaking({ from: sender });
-    await staker.awardStakedBalance(receiver, { from: sender });
-
-    let finalBalance = web3.utils.toBN(await web3.eth.getBalance(receiver));
-    let pastReceivers = await staker.getPastReceivers.call({ from: accounts[0] });
-    let lastReceiver = await staker.getLastReciver.call({ from: accounts[0] });
+    // act + assert
+    await staker.stake({ from: accounts[0], value: amount });
+    await staker.completeStaking({ from: accounts[0] });
+    await staker.restartStaking({ from: accounts[0] });
 
     // assert
-    assert.isTrue(finalBalance.gt(initialBalance));
-    assert.equal(pastReceivers.length, 1);
-    assert.equal(lastReceiver.receiverAddress, accounts[1]);
+    let actual = await staker.getStatus.call({ from: accounts[0] });
+    assert.equal(actual, 0);
   });
 
-  it("should not allow non owner to award staked balance when skating is complete", async () => {
+  it("should restart staking when staking is closed", async () => {
     // arrange
     let amount = web3.utils.toWei('2', 'ether');
 
     // act + assert
     await staker.stake({ from: accounts[0], value: amount });
-    await staker.completeStaking({ from: accounts[0] });
-
-    await Exception.tryCatch(
-      staker.awardStakedBalance(accounts[1], { from: accounts[2] }),
-      Exception.errTypes.nonOwnerCall);
-  });
-
-  it("should restart staking when stake has been awarded", async () => {
-    // arrange
-    let amount = web3.utils.toWei('2', 'ether');
-
-    // act + assert
-    await staker.stake({ from: accounts[0], value: amount });
-    await staker.completeStaking({ from: accounts[0] });
-    await staker.awardStakedBalance(accounts[1], { from: accounts[0] });
+    await staker.closeStaking({ from: accounts[0] });
     await staker.restartStaking({ from: accounts[0] });
 
     // assert
@@ -183,20 +173,50 @@ contract("Staker", function (accounts) {
     // act + assert
     await staker.stake({ from: accounts[0], value: amount });
     await staker.completeStaking({ from: accounts[0] });
-    await staker.awardStakedBalance(accounts[1], { from: accounts[0] });
 
     await Exception.tryCatch(
       staker.restartStaking({ from: accounts[2] }),
       Exception.errTypes.nonOwnerCall);
   });
 
-  it("should restart staking when stake has been awarded", async () => {
-    // act + assert
-    await staker.closeStaking({ from: accounts[0] });
-    await staker.restartStaking({ from: accounts[0] });
+  it("should allow owner to redeem staked balance", async () => {
+    // arrange
+    let amount = web3.utils.toWei('10', 'ether');
+
+    // act
+    await staker.stake({ from: accounts[1], value: amount });
+    await staker.completeStaking({ from: accounts[0] });
+
+    let startingBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
+    await staker.redeemStakedAmount({ from: accounts[0] });
+    let endBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[0]));
 
     // assert
-    let actual = await staker.getStatus.call({ from: accounts[0] });
-    assert.equal(actual, 0);
+    assert.isTrue(endBalance.gt(startingBalance));
+  });
+
+  it("should not allow a non-owner to redeem staked balance", async () => {
+    // arrange
+    let amount = web3.utils.toWei('10', 'ether');
+
+    // act
+    await staker.stake({ from: accounts[1], value: amount });
+    await staker.completeStaking({ from: accounts[0] });
+
+    // assert
+    await Exception.tryCatch(
+      staker.redeemStakedAmount({ from: accounts[1] }), Exception.errTypes.nonOwnerCall);
+  });
+
+  it("should not allow owner to redeem staked balance when staking is not complete", async () => {
+    // arrange
+    let amount = web3.utils.toWei('10', 'ether');
+
+    // act
+    await staker.stake({ from: accounts[1], value: amount });
+
+    // assert
+    await Exception.tryCatch(
+      staker.redeemStakedAmount({ from: accounts[0] }), Exception.errTypes.onlyWhenComplete);
   });
 });
