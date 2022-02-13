5d0f6e7bf050931ca5dfb9a1f16f51b3cbb943bf
Daniel <daniel@bfydigital.com>
2022-02-13 15:11:34 +0100

    removed BFYToken address as a parameter
    BFYToken contract is initialized as the Staker contract is created so that the Staker contract has ownership and can freely award tokens.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const BFYToken = artifacts.require("./BFYToken.sol");
 const Staker = artifacts.require("./Staker.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(BFYToken).then(() => {
-    return deployer.deploy(Staker, BFYToken.address);
-  });
+  deployer.deploy(Staker);
 };
